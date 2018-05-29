96b34f4be9af99578d341c5f123cf657859fe1b9
Tung Dao <me@tungdao.com>
2018-05-29 10:46:37 +0700

    welcome page display unanswered questions

--- api/controllers/dashboard/view-welcome.js
+++ api/controllers/dashboard/view-welcome.js
@@ -1,27 +1,24 @@
+/*global Question*/
+
 module.exports = {
-
-
   friendlyName: 'View welcome page',
-
-
   description: 'Display the dashboard "Welcome" page.',
 
-
   exits: {
-
     success: {
       viewTemplatePath: 'pages/dashboard/welcome',
       description: 'Display the welcome page for authenticated users.'
     },
-
   },
 
-
   fn: async function (inputs, exits) {
-
-    return exits.success();
-
+    const questions = await Question.find({
+      where: {
+        responder: 1,
+        response: ''
+      },
+      sort: 'createdAt DESC'
+    });
+    return exits.success({ questions });
   }
-
-
 };
